00917ee71985be5eaea35beb3f3317bd15a86c52
Mikhail Borisov <mishaborisov2013@gmail.com>
2024-05-12 22:25:36 +0300

    Initial commit

---
+++ Mobius-Demo-SwiftUI/Mobius-Demo-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobius-Demo-SwiftUI
+//
+//  Created by Mikhail Borisov on 12.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
